47843a6fedb3d3edcade82ec5c94eee8c36689c7
Emerson Laurentino <emersonlaurentino@hotmail.com>
2018-03-05 14:51:54 -0300

    feat(pulse): component

--- react-motions/index.js
+++ react-motions/index.js
@@ -3,10 +3,9 @@ export { FadeIn, withFadeIn } from './src/fadeIn'
 export { FadeOut, withFadeOut } from './src/fadeOut'
 export { Flash, withFlash } from './src/flash'
 export { Jello, withJello } from './src/jello'
+export { Pulse, withPulse } from './src/pulse'
 export { Shake, withShake } from './src/shake'
 import withInfinite from './src/withInfinite'
-import withJello from './src/jello/withJello'
-import withPulse from './src/pulse/withPulse'
 import withRubberBand from './src/rubberBand/withRubberBand'
 import withSwing from './src/swing/withSwing'
 import withTada from './src/tada/withTada'
@@ -14,7 +13,6 @@ import withWobble from './src/wobble/withWobble'
 
 export {
   withInfinite,
-  withPulse,
   withRubberBand,
   withSwing,
   withTada,
