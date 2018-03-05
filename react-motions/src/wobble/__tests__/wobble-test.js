678c9eb6c302fc7d7a17cc52ed5b67e4401088f9
Emerson Laurentino <emersonlaurentino@hotmail.com>
2018-03-05 17:14:22 -0300

    feature/Components (#8)
    * fix(package): duplicate object key
    
    * fix(react-motions/package): add dev dependece webpack
    
    * fix(shake): default export
    
    * chore(shake): test
    
    * chore(readme): check component shake
    
    * feat(readme): add shake component on api
    
    * feat(flash): component
    
    * feat(bounce): component
    
    * feat(fadeIn): component
    
    * feat(fadeOut): component
    
    * feat(jello): component
    
    * feat(pulse): component
    
    * feat(rubberBand): component
    
    * feat(swing): component
    
    * feat(tada): component
    
    * feat(wobble): component
    
    * update README.md

--- react-motions/src/wobble/__tests__/wobble-test.js
+++ react-motions/src/wobble/__tests__/wobble-test.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import RubberBand from '../rubberBand';
+import Wobble from '../wobble';
 
-describe('RubberBand', () => {
+describe('Wobble', () => {
   it('should render', () => {
-    const wrapper = shallow(<RubberBand>RubberBand</RubberBand>);
+    const wrapper = shallow(<Wobble>Wobble</Wobble>);
 
     expect(wrapper).toMatchSnapshot();
   });
   it('should receive duration prop', () => {
-    const wrapper = mount(<RubberBand duration={3}>RubberBand</RubberBand>);
+    const wrapper = mount(<Wobble duration={3}>Wobble</Wobble>);
 
     expect(wrapper.props().duration).toEqual(3);
   });
   it('should receive fillMode prop', () => {
-    const wrapper = mount(<RubberBand fillMode="both">RubberBand</RubberBand>);
+    const wrapper = mount(<Wobble fillMode="both">Wobble</Wobble>);
     expect(wrapper.props().fillMode).toEqual("both");
   });
   it('should render correctly when infinite', () => {
-    const wrapper = mount(<RubberBand infinite>RubberBand</RubberBand>);
+    const wrapper = mount(<Wobble infinite>Wobble</Wobble>);
 
     expect(wrapper.props().infinite).toBe(true);
   });
